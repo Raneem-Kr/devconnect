d32812f38ab159934c41dd8eb6d450e6e910c1e6
Raneem <ranim.krenbeh99@gmail.com>
2026-05-25 14:33:29 +0200

    fix login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://devconnect-pxxq.onrender.com/api/auth/login",
         formData,
       );
 
